ad4d6f40ccb52380e8c7fb59a42f296eacadf00e
JJ Ben-Joseph <jj@tensorspace.ai>
2024-02-22 15:33:51 +0200

    Update docstrings.

--- aiondata/bindingdb.py
+++ aiondata/bindingdb.py
@@ -10,6 +10,11 @@ from .datasets import GeneratedDataset
 
 
 class BindingDB(GeneratedDataset):
+    """BindingDB
+
+    A public, web-accessible database of measured binding affinities, focusing chiefly on the interactions of protein considered to be drug-targets with small, drug-like molecules.
+    """
+
     SOURCE = "https://www.bindingdb.org/bind/downloads/BindingDB_All_3D_202402_sdf.zip"
 
     float_fields = {
@@ -22,6 +27,13 @@ class BindingDB(GeneratedDataset):
     }
 
     def __init__(self, fd: Optional[io.BytesIO] = None):
+        """
+        Initializes a BindingDB instance.
+
+        Args:
+            fd (Optional[io.BytesIO]): The file-like object containing the dataset content.
+                If `fd` is not provided, the dataset content will be fetched from the default source.
+        """
         if fd is None:
             self.fd = self.from_url(self.SOURCE)
         else:
@@ -29,6 +41,17 @@ class BindingDB(GeneratedDataset):
 
     @staticmethod
     def _convert_to_numeric(prop_name: str, value: str):
+        """
+        Converts a property value to numeric type.
+
+        Args:
+            prop_name (str): The name of the property.
+            value (str): The value of the property.
+
+        Returns:
+            The converted numeric value, or None if conversion fails.
+
+        """
         if prop_name in BindingDB.float_fields:
             try:
                 return float(value)
@@ -46,6 +69,15 @@ class BindingDB(GeneratedDataset):
 
     @staticmethod
     def from_url(url: str) -> "BindingDB":
+        """
+        Creates a BindingDB instance from a URL containing a compressed SDF file (typically upstream).
+
+        Args:
+            url (str): The URL of the dataset.
+
+        Returns:
+            A BindingDB instance.
+        """
         with urllib.request.urlopen(url) as response:
             with zipfile.ZipFile(io.BytesIO(response.read())) as z:
                 sdf_name = z.namelist()[0]
@@ -55,11 +87,29 @@ class BindingDB(GeneratedDataset):
 
     @staticmethod
     def from_uncompressed_file(file_path: str) -> "BindingDB":
+        """
+        Creates a BindingDB instance from an uncompressed SDF file.
+
+        Args:
+            file_path (str): The path to the uncompressed file.
+
+        Returns:
+            A BindingDB instance.
+        """
         with open(file_path, "rb") as f:
             file_content = io.BytesIO(f.read())
         return BindingDB(file_content)
 
     def to_generator(self, progress_bar: bool = True):
+        """
+        Converts the dataset to a generator.
+
+        Args:
+            progress_bar (bool): Whether to display a progress bar.
+
+        Yields:
+            dict: A dictionary representing a record in the dataset.
+        """
         RDLogger.DisableLog("rdApp.*")  # Suppress RDKit warnings and errors
 
         sd = Chem.ForwardSDMolSupplier(self.fd, sanitize=False, removeHs=False)
