0a3f4d96e69eb7ed67156662ca327efa58b59d84
JJ Ben-Joseph <jj@tensorspace.ai>
2024-03-12 14:27:00 +0200

    Fix test and refactor into pytest from unittest.

--- tests/PDB_test.py
+++ tests/PDB_test.py
@@ -1,63 +1,52 @@
-#set current directory to the parent directory
-
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-import sys
-sys.path.append('..')
-
-import pypdb 
-from aiondata import PDBHandler
-import unittest
-from unittest import mock
+# Import necessary modules
+import pytest
 from unittest.mock import patch
-import pypdb.clients.search.search_client
+import pypdb
+from aiondata import PDBHandler
 
 
+@pytest.fixture
+def pdb_handler():
+    return PDBHandler()
 
 
-
-class TestPDBHandler(unittest.TestCase):
-
-    def setUp(self):
-        self.pdb_handler = PDBHandler()
-
-    def test_get_pdb(self):
-        pdb_ids = ['8IRB', '100D']
-        with patch.object(self.pdb_handler.pdb_list, 'retrieve_pdb_file') as mock_retrieve_pdb_file:
-            self.pdb_handler.get_pdb(pdb_ids)
-            mock_retrieve_pdb_file.assert_any_call(pdb_ids[0], pdir=self.pdb_handler.save_dir, file_format='pdb')
-            mock_retrieve_pdb_file.assert_any_call(pdb_ids[1], pdir=self.pdb_handler.save_dir, file_format='pdb')
-
-
-    def test_get_pdb_info(self):
-        pdb_id = '4OS0'
-        with patch.object(pypdb, 'get_all_info') as mock_get_all_info:
-            self.pdb_handler.get_pdb_info(pdb_id)
-            mock_get_all_info.assert_called_with(pdb_id)
+def test_get_pdb(pdb_handler):
+    pdb_ids = ["8IRB", "100D"]
+    with patch.object(
+        pdb_handler.pdb_list, "retrieve_pdb_file"
+    ) as mock_retrieve_pdb_file:
+        pdb_handler.get_pdb(pdb_ids)
+        mock_retrieve_pdb_file.assert_any_call(
+            pdb_ids[0], pdir=pdb_handler.save_dir, file_format="pdb"
+        )
+        mock_retrieve_pdb_file.assert_any_call(
+            pdb_ids[1], pdir=pdb_handler.save_dir, file_format="pdb"
+        )
 
 
-    def test_searchpdb(self):
-        title = 'Solution'
-        organism = '9606'
-        Uniprot_accession = "P04637"
-        fromdb = "UniProt"
-        nonpolymer = 1
-        ComparisonType = "Less"
-
-        with patch('pypdb.clients.search.search_client.perform_search_with_graph') as mock_perform_search_with_graph:
-                self.pdb_handler.searchpdb(title=title,
-                                        organism=organism,
-                                        Uniprot_accession=Uniprot_accession,
-                                        fromdb=fromdb,
-                                        nonpolymer=nonpolymer,
-                                        ComparisonType=ComparisonType
-                                        )
-                mock_perform_search_with_graph.assert_called()
-                #self.assertTrue(mock_perform_search_with_graph.called, "The perform_search_with_graph function was not called")
-                #mock_perform_search_with_graph.assert_called_with(query_object=mock.ANY, return_type=mock.ANY)
+def test_get_pdb_info(pdb_handler):
+    pdb_id = "4OS0"
+    with patch.object(pypdb, "get_all_info") as mock_get_all_info:
+        pdb_handler.get_pdb_info(pdb_id)
+        mock_get_all_info.assert_called_with(pdb_id)
 
 
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_searchpdb(pdb_handler):
+    title = "Solution"
+    organism = "9606"
+    Uniprot_accession = "P04637"
+    fromdb = "UniProt"
+    nonpolymer = 1
+    ComparisonType = "Less"
+    with patch(
+        "aiondata.protein_structure.perform_search_with_graph"
+    ) as mock_perform_search_with_graph:
+        pdb_handler.searchpdb(
+            title=title,
+            organism=organism,
+            Uniprot_accession=Uniprot_accession,
+            fromdb=fromdb,
+            nonpolymer=nonpolymer,
+            ComparisonType=ComparisonType,
+        )
+        mock_perform_search_with_graph.assert_called()
