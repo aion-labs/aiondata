bd9ed44528d70b5db1162dfc9d3fc9884069e409
JJ Ben-Joseph <jj@tensorspace.ai>
2024-03-12 11:19:20 +0200

    Start data sublogic.

--- aiondata/weizmann_ccca.py
+++ aiondata/weizmann_ccca.py
@@ -10,3 +10,11 @@ from .datasets import ParquetDataset
 class Weizman3CA(ParquetDataset):
     COLLECTION = "weizmann_ccca"
     SOURCE = "https://raw.githubusercontent.com/aion-labs/aiondata/main/data/3ca_links.parquet"
+
+    def __getitem__(self, study_name_to_find: str):
+        links = self.to_df()
+        row = links.filter(pl.col("Study name") == study_name_to_find)
+        if row.height == 0:
+            raise ValueError(
+                f"Study name {study_name_to_find} not found in the dataset."
+            )
